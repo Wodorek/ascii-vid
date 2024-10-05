327972eea2992f6720f926c65a053a232944d943
Wodorek <krol.w94@gmail.com>
2024-10-05 18:27:51 +0200

    pixelization complete

--- src/Video_Processor.py
+++ src/Video_Processor.py
@@ -4,7 +4,7 @@ import cv2 as cv
 class VideoProcessor():
     def __init__(self):
         # add selecting tile size
-        self.tile_size = 20
+        self.tile_size = 60
         self.vid = None
 
     def set_tile_size(self, size):
@@ -16,7 +16,6 @@ class VideoProcessor():
 
     def show_video(self):
 
-        print(self.vid)
         if not self.vid:
             print("no video selected")
             return
@@ -27,18 +26,38 @@ class VideoProcessor():
             print("Error opening stream or file")
             return
 
+        height = video.get(cv.CAP_PROP_FRAME_HEIGHT)
+        width = video.get(cv.CAP_PROP_FRAME_WIDTH)
+        frame_rate = video.get(cv.CAP_PROP_FPS)
+
         while (video.isOpened()):
             ret, frame = video.read()
 
             if ret != True:
                 break
 
-            grey = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+            pixelated = self.process_frame(frame, width, height)
 
-            cv.imshow('Frame', grey)
+            cv.imshow('Frame', pixelated)
 
             if cv.waitKey(25) & 0xFF == ord('q'):
                 break
 
         video.release()
         cv.destroyAllWindows()
+
+    def process_frame(self, frame, frame_w, frame_h):
+
+        print(frame)
+
+        new_width = int(frame_w // self.tile_size)
+        new_height = int(frame_h // self.tile_size)
+
+        print(new_height, new_width)
+
+        tmp_frame = cv.resize(frame, (new_width, new_height),
+                              interpolation=cv.INTER_LINEAR)
+
+        output_frame = cv.resize(
+            tmp_frame, (int(frame_w), int(frame_h)), interpolation=cv.INTER_NEAREST)
+        return output_frame
